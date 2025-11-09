4e6c878ffd5a48d698bbf42b4d889a7cb53bb01b
Dickla Ncube <Dickla.Ncube@sanbs.org.za>
2025-11-09 15:42:01 +0200

    ADDEDD

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        textAlign: 'center',
+        backgroundColor: '#e0f7fa',
+        padding: '20px',
+        margin: '10px',
+        borderRadius: '8px'
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
+
+  
