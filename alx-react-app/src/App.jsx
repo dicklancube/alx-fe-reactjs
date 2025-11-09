37f0acd119687d9e139724f086f7b10c757f4150
Dickla Ncube <Dickla.Ncube@sanbs.org.za>
2025-11-09 15:28:32 +0200

    Updated App.jsx to include UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
@@ -15,6 +16,11 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile
+        name="Dickla Ncube"
+        age="25"
+        bio="Loves hiking and photography"
+      />
       <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
