a941454140f2e680cab99eb8c42552b4720f3eba
Dickla Ncube <Dickla.Ncube@sanbs.org.za>
2025-11-09 15:16:22 +0200

    added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +12,10 @@ function App() {
 
   return (
     <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
